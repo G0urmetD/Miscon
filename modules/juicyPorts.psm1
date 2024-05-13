73116d42717bea9f8466e8eac2f51d97e0daf174
G0urmet <60777262+G0urmetD@users.noreply.github.com>
2024-05-13 11:25:34 +0200

    Update juicyPorts.psm1

--- modules/juicyPorts.psm1
+++ modules/juicyPorts.psm1
@@ -12,6 +12,11 @@ function Test-JuicyPorts {
 
     $results = @()
 
+    Write-Host -ForegroundColor Yellow "[INFO]" -NoNewline
+    Write-Host " Fetching computer objects out of the domain."
+    Write-Host -ForegroundColor Yellow "[INFO]" -NoNewline
+    Write-Host " Scan for juicy ports."
+
     foreach ($computer in $computers) {
         $computerName = $computer.Name
         $ipAddress = [System.Net.Dns]::GetHostAddresses($computerName) | Where-Object { $_.AddressFamily -eq "InterNetwork" } | Select-Object -ExpandProperty IPAddressToString
@@ -21,18 +26,31 @@ function Test-JuicyPorts {
         foreach ($port in $ports) {
             $tcpClient = New-Object System.Net.Sockets.TcpClient
             $result = $tcpClient.BeginConnect($computerName, $port, $null, $null)
-            $result.AsyncWaitHandle.WaitOne(1000, $false)
+            $result.AsyncWaitHandle.WaitOne(1000, $false) | Out-Null
 
             if ($tcpClient.Connected) {
                 $openPorts += $port
                 $tcpClient.Close()
             }
+
+            $tcpClient.Dispose()
+        }
+
+        $hint = @()
+
+        if(5985 -in $openPorts) {
+            $hint += "[INFO] WinRM should be used with HTTPS/5986. Happy evil-winrm ;)"
+        }
+        
+        if(3389 -in $openPorts) {
+            $hint += "[INFO] RDP on clients should be disabled. Happy connection ;)"
         }
 
         $resultObject = [PSCustomObject]@{
             Hostname = $computerName
-            'IP-Adresse' = $ipAddress
-            'Offene Ports' = $openPorts -join ', '
+            'IP-Address' = $ipAddress
+            'juicy Ports' = $openPorts -join ', '
+            Hint = $hint -join ', '
         }
 
         $results += $resultObject
